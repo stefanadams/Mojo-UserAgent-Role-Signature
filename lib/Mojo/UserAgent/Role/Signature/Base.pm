de509eb5ad8a26a308073c8c9a40cc53695f3387
Stefan Adams <s1037989@gmail.com>
2020-02-27 06:55:51 -0600

    Add transaction-based proxy detection support

--- lib/Mojo/UserAgent/Role/Signature/Base.pm
+++ lib/Mojo/UserAgent/Role/Signature/Base.pm
@@ -1,11 +1,20 @@
 package Mojo::UserAgent::Role::Signature::Base;
 use Mojo::Base -base;
 
+use Mojo::UserAgent::Proxy;
 use Mojo::Transaction::HTTP;
 
 has 'name';
+has proxy => sub { Mojo::UserAgent::Proxy->new };
 has tx => sub { Mojo::Transaction::HTTP->new };
 
 sub sign_tx { shift->tx }
+
+sub use_proxy {
+  my $self = shift;
+  local $ENV{MOJO_PROXY} = 1;
+  $self->proxy->prepare($self->tx);
+  return $self->tx;
+}
 
 1;
